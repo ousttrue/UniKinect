ca0832c821a29d210bfe9ea4a112b54bc2337d4c
ousttrue <ousttrue@gmail.com>
2020-01-27 13:39:08 +0900

    depth range

--- UnityProject/Assets/UniKinect/Scripts/KinectSensor.cs
+++ UnityProject/Assets/UniKinect/Scripts/KinectSensor.cs
@@ -12,9 +12,23 @@ public class KinectSensor : MonoBehaviour
     long _lastTime;
     public Texture2D DepthTexture;
     Int16[] _depth;
-    Color32[] _colors;
+    byte[] _rawTexture;
+    public uint MinDepth;
+    public uint MaxDepth;
+    [Range(0.1f, 10.0f)]
+    public float Factor = 255.0f / 1000;
+
+    [Range(0, 800)]
+    public uint DepthLimitCM = 800;
 
     public bool CopyTexture = true;
+    const uint PLAYER_MASK = 0x7;
+    const int PLAYER_MASK_WIDTH = 3;
+
+    void OnValidate()
+    {
+        MaxDepth = 0;
+    }
 
     // Update is called once per frame
     void Update()
@@ -53,11 +67,11 @@ public class KinectSensor : MonoBehaviour
             if (DepthTexture == null)
             {
                 DepthTexture = new Texture2D(
-                    _width, _height, TextureFormat.ARGB32, false);
+                    _width, _height, TextureFormat.BGRA32, false);
                 DepthTexture.filterMode = FilterMode.Point;
                 var length = _width * _height;
                 _depth = new Int16[length];
-                _colors = new Color32[length];
+                _rawTexture = new byte[length * 4];
             }
 
             if (!CopyTexture)
@@ -65,18 +79,37 @@ public class KinectSensor : MonoBehaviour
                 return;
             }
 
-            frame.AccessUnderlyingBuffer(out uint bufferSize, out IntPtr buffer).ThrowIfFailed();
-            Marshal.Copy(buffer, _depth, 0, _depth.Length);
-            for (int i = 0; i < _depth.Length; ++i)
+            // get depth buffer ptr
+            frame.AccessUnderlyingBuffer(out uint bufferSize, out IntPtr ptr).ThrowIfFailed();
+            // copy depth buffer to array
+            Marshal.Copy(ptr, _depth, 0, _depth.Length);
+
+            frame.get_DepthMinReliableDistance(out ushort min);
+            frame.get_DepthMaxReliableDistance(out ushort max);
+
+            MinDepth = ushort.MaxValue;
+            // MaxDepth = 0;
+            var span = MemoryMarshal.Cast<byte, uint>(_rawTexture.AsSpan());
+            var p = 0;
+            Factor = 1.0f / MaxDepth * 255;
+            for (int i = 0; i < _depth.Length; ++i, p += 4)
             {
-                var d = _depth[i];
-                var b = (Byte)(UInt16)d;
-                _colors[i] =
-                new Color32(
-                    b, b, b, 255
-                );
+                var value = _depth[i];
+                var player = value & PLAYER_MASK;
+                var depth = (uint)(value >> PLAYER_MASK_WIDTH);
+                if (depth > MaxDepth && depth < DepthLimitCM) MaxDepth = depth;
+                else if (depth < MinDepth && depth > 0) MinDepth = depth;
+                if (!float.IsInfinity(Factor))
+                {
+                    var intencity = (byte)(depth * Factor);
+                    _rawTexture[p] = intencity;
+                    _rawTexture[p + 1] = intencity;
+                    _rawTexture[p + 2] = intencity;
+                    _rawTexture[p + 3] = 255;
+                }
+                // span[i] = depth | 0xFF;
             }
-            DepthTexture.SetPixels32(_colors);
+            DepthTexture.LoadRawTextureData(_rawTexture);
             DepthTexture.Apply();
         }
     }
